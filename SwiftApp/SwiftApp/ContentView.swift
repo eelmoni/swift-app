a4c41facd0c81ca3bd93682ec0ddb276dbfc1a91
Emmanuel Elmoni <emmanuel.elmoni@gmail.com>
2022-05-31 17:50:11 -0300

    Initial commit

---
+++ SwiftApp/SwiftApp/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  SwiftApp
+//
+//  Created by Distillery on 31/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
+            Text("89")
+            HStack {
+                Text("1")
+                Slider(value: .constant(50), in: 1.0...100.00)
+                Text("100")
+            }
+            Button(action: {}) {
+                Text("Hit me")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
